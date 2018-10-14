7272352c7747d474860af1719b19cb6fef18cd4a
okeydokey <yunkim28@gmail.com>
2018-10-14 19:33:53 +0900

    Authorization Server 설정 추가

--- src/main/java/com/okeydokey/oauth2study/config/SecurityConfig.java
+++ src/main/java/com/okeydokey/oauth2study/config/SecurityConfig.java
@@ -15,6 +15,7 @@ import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
 import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
 import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
@@ -26,6 +27,7 @@ import java.util.List;
 
 @Configuration
 @EnableOAuth2Client
+@EnableAuthorizationServer
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
