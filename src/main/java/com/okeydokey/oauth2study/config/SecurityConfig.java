2ed8744b0125fd18bb72fd645804b7de2bedcfd4
okeydokey <yunkim28@gmail.com>
2018-10-14 17:28:19 +0900

    CSRF 설정 추가

--- src/main/java/com/okeydokey/oauth2study/config/SecurityConfig.java
+++ src/main/java/com/okeydokey/oauth2study/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 @EnableOAuth2Sso
@@ -11,11 +12,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-            .antMatcher("/**")
-            .authorizeRequests()
-            .antMatchers("/", "/login**", "/webjars/**", "/error**")
-            .permitAll()
-            .anyRequest()
-            .authenticated();
+                .antMatcher("/**")
+                .authorizeRequests()
+                .antMatchers("/", "/login**", "/webjars/**", "/error**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .logout().logoutSuccessUrl("/")
+                .permitAll()
+                .and()
+                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+
+        ;
     }
 }
