0d142c996cc6dcff3246b0fa4d1abda7af481574
okeydokey <yunkim28@gmail.com>
2018-10-14 16:44:56 +0900

    EnableOAuthSso 설정, configuration 추가

--- src/main/java/com/okeydokey/oauth2study/Oauth2StudyApplication.java
+++ src/main/java/com/okeydokey/oauth2study/Oauth2StudyApplication.java
@@ -2,8 +2,10 @@ package com.okeydokey.oauth2study;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 
 @SpringBootApplication
+@EnableOAuth2Sso
 public class Oauth2StudyApplication {
     public static void main(String[] args) {
         SpringApplication.run(Oauth2StudyApplication.class, args);
