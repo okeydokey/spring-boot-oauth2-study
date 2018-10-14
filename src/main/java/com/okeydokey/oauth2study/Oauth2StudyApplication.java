d8ab60dab3a1c528e056e96d6bc84f24bf4ac7b0
okeydokey <yunkim28@gmail.com>
2018-10-14 16:37:30 +0900

    first commit

---
+++ src/main/java/com/okeydokey/oauth2study/Oauth2StudyApplication.java
@@ -0,0 +1,11 @@
+package com.okeydokey.oauth2study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2StudyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2StudyApplication.class, args);
+    }
+}
---
+++ src/test/java/com/okeydokey/oauth2study/Oauth2StudyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.okeydokey.oauth2study;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Oauth2StudyApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
